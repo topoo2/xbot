fb298d05ae58c269b7c3a5aca4366e469f65c3e8
ToPoO <64712757+topoo2@users.noreply.github.com>
2026-07-14 03:47:01 +0100

    Modify bug report button label and URL
    Updated the label and URL for the bug report button.

--- src/handlers/help/helpButtons.js
+++ src/handlers/help/helpButtons.js
@@ -41,9 +41,9 @@ export const helpBugReportButton = {
     name: BUG_REPORT_BUTTON_ID,
     async execute(interaction, client) {
         const githubButton = new ButtonBuilder()
-            .setLabel('🐛 Report Bug on GitHub')
+            .setLabel('🐛 Report Bug ')
             .setStyle(ButtonStyle.Link)
-            .setURL('https://github.com/codebymitch/TitanBot/issues');
+            .setURL('Hna knghelto hhhhhh');
 
         const bugRow = new ActionRowBuilder().addComponents(githubButton);
 
